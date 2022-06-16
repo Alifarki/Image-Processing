0a7b2b0f932dee6a710c6f6abb8357ea6d33f461
Ali Farki <51064034+Alifarki@users.noreply.github.com>
2022-06-16 08:05:54 -0700

    Delete IntensityTransformation directory

--- IntensityTransformation/Negative.py
+++
@@ -1,7 +0,0 @@
-import cv2
-img=cv2.imread("./images/img2.jpg")
-img=img[:,:,0]
-imgd=cv2.normalize(img.astype('float'),None,0.0,1.0,cv2.NORM_MINMAX)
-img_neg=1-imgd
-cv2.imshow("negative",img_neg)
-cv2.waitKey(0)
